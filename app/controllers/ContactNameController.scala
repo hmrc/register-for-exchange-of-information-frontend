3d096eb905229720e65032fce0a610a464548fc8
mbiczak <13977957+mbiczak@users.noreply.github.com>
2021-09-01 09:47:20 +0100

    DAC6-1000 synchronized with latest DAC6-999

--- app/controllers/ContactNameController.scala
+++ app/controllers/ContactNameController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import controllers.actions._
@@ -20,44 +36,47 @@ import uk.gov.hmrc.viewmodels.NunjucksSupport
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class ContactNameController @Inject()(
-    override val messagesApi: MessagesApi,
-    sessionRepository: SessionRepository,
-    navigator: Navigator,
-    identify: IdentifierAction,
-    getData: DataRetrievalAction,
-    requireData: DataRequiredAction,
-    formProvider: ContactNameFormProvider,
-    val controllerComponents: MessagesControllerComponents,
-    renderer: Renderer
-)(implicit ec: ExecutionContext) extends FrontendBaseController with I18nSupport with NunjucksSupport {
+class ContactNameController @Inject() (
+  override val messagesApi: MessagesApi,
+  sessionRepository: SessionRepository,
+  navigator: Navigator,
+  identify: IdentifierAction,
+  getData: DataRetrievalAction,
+  requireData: DataRequiredAction,
+  formProvider: ContactNameFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  renderer: Renderer
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport
+    with NunjucksSupport {
 
   private val form = formProvider()
 
   private def render(mode: Mode, form: Form[String])(implicit request: DataRequest[AnyContent]): Future[Html] = {
     val data = Json.obj(
-      "form"       -> form,
-      "action"     -> routes.ContactNameController.onSubmit(mode).url
+      "form"   -> form,
+      "action" -> routes.ContactNameController.onSubmit(mode).url
     )
     renderer.render("contactName.njk", data)
   }
 
   def onPageLoad(mode: Mode): Action[AnyContent] = (identify andThen getData.apply andThen requireData).async {
     implicit request =>
-
       render(mode, request.asForm(pages.ContactNamePage, form)).map(Ok(_))
   }
 
   def onSubmit(mode: Mode): Action[AnyContent] = (identify andThen getData.apply andThen requireData).async {
     implicit request =>
-
-      form.bindFromRequest().fold(
-        formWithErrors => render(mode, formWithErrors).map(BadRequest(_)),
-        value =>
-          for {
-            updatedAnswers <- request.update(ContactNamePage, value)
-            _              <- sessionRepository.set(updatedAnswers)
-          } yield Redirect(navigator.nextPage(ContactNamePage)(DefaultJourney(mode))(Option(value)))
-      )
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => render(mode, formWithErrors).map(BadRequest(_)),
+          value =>
+            for {
+              updatedAnswers <- request.update(ContactNamePage, value)
+              _              <- sessionRepository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(ContactNamePage)(DefaultJourney(mode))(Option(value)))
+        )
   }
 }
