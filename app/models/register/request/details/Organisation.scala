df6b7350ccdd88f8e698f95faa1eae54dd9c83c1
mbiczak <13977957+mbiczak@users.noreply.github.com>
2021-11-19 16:44:18 +0000

    DAC6-1150

--- app/models/register/request/details/Organisation.scala
+++ app/models/register/request/details/Organisation.scala
@@ -17,7 +17,7 @@
 package models.register.request.details
 
 import models.BusinessType
-import play.api.libs.json.{Json, OWrites, Reads, __}
+import play.api.libs.json.{__, Json, OWrites, Reads}
 
 case class Organisation(organisationName: String, organisationType: BusinessType)
 
