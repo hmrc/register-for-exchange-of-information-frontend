fd19b62850ab69b4be0c91484ea119843fcc2782
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2022-01-31 09:59:07 +0000

    DAC6-1218: reformat

--- test/models/email/EmailUserTypeSpec.scala
+++ test/models/email/EmailUserTypeSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models.email
 
 import base.SpecBase
@@ -28,8 +44,7 @@ class EmailUserTypeSpec extends SpecBase {
 
         emailUserType.getUserTypeFromUa(userAnswers) mustBe SoleTrader
       }
-      "must return Organisation when UserAnswers has WhatAreYouRegisteringAs RegistrationTypeBusiness " in
-      {
+      "must return Organisation when UserAnswers has WhatAreYouRegisteringAs RegistrationTypeBusiness " in {
         val userAnswers = UserAnswers(userAnswersId)
           .set(DoYouHaveUniqueTaxPayerReferencePage, false)
           .success
@@ -40,8 +55,7 @@ class EmailUserTypeSpec extends SpecBase {
 
         emailUserType.getUserTypeFromUa(userAnswers) mustBe Organisation
       }
-      "must return Individual when UserAnswers has WhatAreYouRegisteringAs RegistrationTypeBusiness " in
-      {
+      "must return Individual when UserAnswers has WhatAreYouRegisteringAs RegistrationTypeBusiness " in {
         val userAnswers = UserAnswers(userAnswersId)
           .set(DoYouHaveUniqueTaxPayerReferencePage, false)
           .success
@@ -52,8 +66,7 @@ class EmailUserTypeSpec extends SpecBase {
 
         emailUserType.getUserTypeFromUa(userAnswers) mustBe Individual
       }
-      "must return Organisation when UserAnswers has BusinessTypePage containing something other than Sole " in
-      {
+      "must return Organisation when UserAnswers has BusinessTypePage containing something other than Sole " in {
         val userAnswers = UserAnswers(userAnswersId)
           .set(DoYouHaveUniqueTaxPayerReferencePage, true)
           .success
