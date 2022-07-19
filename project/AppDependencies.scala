558e4a529bc683a7f1ea821f09c6228000973718
Mahipal Padala <22635253+mahipal-padala@users.noreply.github.com>
2022-07-19 12:14:10 +0100

    DAC6-1897: Updated bootstrap-frontend-play-28 and sbt-plugin (#192)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.13.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.11.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "5.21.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % "5.24.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % "0.62.0",
     "uk.gov.hmrc"       %% "logback-json-logger"           % "5.1.0",
     "uk.gov.hmrc"       %% "bootstrap-health-play-28"      % "5.21.0",
