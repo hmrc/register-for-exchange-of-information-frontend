b5f9df1083a9f5e00b6093899ce44bed37fabcc7
Brian Wakenshaw <36340167+BriWak@users.noreply.github.com>
2022-12-05 16:19:19 +0000

    DAC6-2180: removed unused imports

--- test/controllers/IndividualHaveContactTelephoneControllerSpec.scala
+++ test/controllers/IndividualHaveContactTelephoneControllerSpec.scala
@@ -18,14 +18,10 @@ package controllers
 
 import base.ControllerSpecBase
 import models.{NormalMode, UserAnswers}
-import org.mockito.ArgumentCaptor
 import org.mockito.ArgumentMatchers.any
 import pages.IndividualHaveContactTelephonePage
-import play.api.libs.json.{JsObject, Json}
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import play.twirl.api.Html
-import uk.gov.hmrc.viewmodels.Radios
 import views.html.IndividualHaveContactTelephoneView
 
 import scala.concurrent.Future
