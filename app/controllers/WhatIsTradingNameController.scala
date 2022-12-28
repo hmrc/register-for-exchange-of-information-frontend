14ad882ecbc8725214faa05977ba38f3abc1264d
AashuChauhanHmrc <aashu.chauhan@digital.hmrc.gov.uk>
2022-12-28 14:55:30 +0000

    Added controller

--- app/controllers/WhatIsTradingNameController.scala
+++ app/controllers/WhatIsTradingNameController.scala
@@ -31,6 +31,7 @@ import renderer.Renderer
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import uk.gov.hmrc.viewmodels.NunjucksSupport
+import views.html.WhatIsTradingNameView
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
