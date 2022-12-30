225bda4e633a7963125c5b66578145be553863b8
harryhamilton <8810902+HarryHamilton@users.noreply.github.com>
2022-12-30 13:33:57 +0000

    Fix tests

--- test/controllers/YourContactDetailsControllerSpec.scala
+++ test/controllers/YourContactDetailsControllerSpec.scala
@@ -16,19 +16,24 @@
 
 package controllers
 
-import base.SpecBase
+import base.{ControllerMockFixtures, ControllerSpecBase, SpecBase}
 import models.NormalMode
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import views.html.YourContactDetailsView
 
-class YourContactDetailsControllerSpec extends SpecBase {
+class YourContactDetailsControllerSpec extends SpecBase with ControllerMockFixtures {
+
+  lazy val loadRoute   = routes.YourContactDetailsController.onPageLoad(NormalMode).url
+  lazy val submitRoute = routes.YourContactDetailsController.onPageLoad(NormalMode).url
 
   "YourContactDetails Controller" - {
 
     "must return OK and the correct view for a GET" in {
 
-      val application = applicationBuilder(userAnswers = Some(emptyUserAnswers)).build()
+      retrieveUserAnswersData(emptyUserAnswers)
+
+      val application = guiceApplicationBuilder().build()
 
       running(application) {
         val request = FakeRequest(GET, routes.YourContactDetailsController.onPageLoad(NormalMode).url)
