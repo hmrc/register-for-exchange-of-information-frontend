4c85b03dd4cffaa8ce79fb717d6e8fd6c594b12d
Mark Reynolds <20478032+ers2015112@users.noreply.github.com>
2023-09-28 12:02:31 +0100

    DAC6-2844: Add constraints and tests

--- app/forms/mappings/Constraints.scala
+++ app/forms/mappings/Constraints.scala
@@ -55,6 +55,18 @@ trait Constraints {
         }
     }
 
+  protected def inRange[A](minimum: A, maximum: A, errorKey: String)(implicit ev: Ordering[A]): Constraint[A] =
+    Constraint {
+      input =>
+        import ev._
+
+        if (input >= minimum && input <= maximum) {
+          Valid
+        } else {
+          Invalid(errorKey, minimum, maximum)
+        }
+    }
+
   protected def regexp(regex: String, errorKey: String): Constraint[String] =
     Constraint {
       case str if str.matches(regex) =>
@@ -87,4 +99,11 @@ trait Constraints {
         Valid
     }
 
+  protected def nonEmptySet(errorKey: String): Constraint[Set[_]] =
+    Constraint {
+      case set if set.nonEmpty =>
+        Valid
+      case _ =>
+        Invalid(errorKey)
+    }
 }
