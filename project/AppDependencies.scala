d8abd70235581f7416eb7378ee5f7a272e5df015
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2026-03-13 14:25:43 +0000

    DDCYLS-8142: dependency upgrades for vulnerabilities (#342)
    * dependency upgrades for vulnerabilities
    
    * PR Bot Comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.5.0"
+  private val bootstrapVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.29.0",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.4.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "12.32.0",
+    "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % "2.12.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "13.0.0",
