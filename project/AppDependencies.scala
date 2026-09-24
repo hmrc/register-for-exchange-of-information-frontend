152a86b9ff24f90c146943981b7bbd4445e2b511
AbdulHQuadri <67271481+AbdulHQuadri@users.noreply.github.com>
2026-09-24 11:26:17 +0100

    User research banner

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,10 +6,10 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.9.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"            % "13.14.0",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"            % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % "2.13.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                    % "2.14.0",
     "uk.gov.hmrc"       %% "domain-play-30"                        % "13.0.0",
     "org.typelevel"     %% "cats-core"                             % "2.10.0"
   )
